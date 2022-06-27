9d85c80b80603bab00a46d4aa8b10e01bb74e112
freedevsoft <tkahn0430@gmail.com>
2022-06-27 20:58:50 +0300

    added home screen

---
+++ src/components/BottomBar/Copyright.tsx
@@ -0,0 +1,12 @@
+import { Typography, Box } from '@mui/material';
+
+export const Copyright = () => {
+  return (
+    <>
+      <Box mb={3}>
+        <Typography fontSize={8}>© 2022 INK Games™. All Rights Reserved.</Typography>
+        <Typography fontSize={8} mt={1}>Reproduction without permission is prohibited.</Typography>
+      </Box>
+    </>
+  );
+};
