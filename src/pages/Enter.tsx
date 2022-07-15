dc2afc56273155d8686e02e1647c2cf9a9582710
freedevsoft <tkahn0430@gmail.com>
2022-07-15 17:59:44 +0300

    finished CI/CD

--- src/pages/Enter.tsx
+++ src/pages/Enter.tsx
@@ -23,7 +23,7 @@ export const Enter = () => {
         <TypeWrapper>
           <Typewriter
             onInit={(typewriter) => {
-              typewriter.typeString('ENTER THE INKLABS').start();
+              typewriter.typeString('ENTER THE LAB').start();
             }}
             options={{
               autoStart: true,
